4d44c4228a05996b277513eebeace67988c5e6c1
Andrey Loginov <andrey12q112@gmail.com>
2024-03-04 23:27:44 +0300

    feat: add gql schema

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,11 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLSchema } from 'graphql';
+import { UserObjectType } from './user/user.type.js';
+import { PostObjectType } from './post/post.type.js';
+import { MemberObjectType } from './member/member.type.js';
+import { ProfileObjectType } from './profile/profile.type.js';
+import { MutationType } from './types/mutation.js';
+import { QueryType } from './types/query.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +25,9 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+export const gqlSchema: GraphQLSchema = new GraphQLSchema({
+  query: QueryType,
+  mutation: MutationType,
+  types: [UserObjectType, PostObjectType, MemberObjectType, ProfileObjectType],
+});
