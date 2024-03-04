fc8f1f270efafb6088cf55d0abe3f3b94229ac25
Andrey Loginov <andrey12q112@gmail.com>
2024-03-04 17:46:41 +0300

    feat: add context interface

---
+++ src/routes/graphql/types/context.ts
@@ -0,0 +1,9 @@
+import type { PrismaClient } from '@prisma/client';
+import type { TLoaders } from './loaders.js';
+import type { FastifyInstance } from 'fastify';
+
+export interface IGraphQLContext {
+  fastify: FastifyInstance;
+  prisma: PrismaClient;
+  loaders: TLoaders;
+};
